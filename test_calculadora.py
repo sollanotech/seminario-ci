05914ed02f20edf81c74b4ce32462534bc4aaf71
solanotech <sollanocogo@gmail.com>
2026-08-13 10:11:43 -0300

    Primeiro commit

---
+++ test_calculadora.py
@@ -0,0 +1,13 @@
+from calculadora import soma, subtracao, multiplicacao
+
+
+def test_soma():
+    assert soma(2, 3) == 5
+
+
+def test_subtracao():
+    assert subtracao(5, 3) == 2
+
+
+def test_multiplicacao():
+    assert multiplicacao(4, 3) == 12
